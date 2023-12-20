0829e2d00aa405e99afb31fbda63655469b96a57
Preetam Jain <ppj36825@uga.edu>
2023-12-19 19:26:19 -0500

    desktop/non-response version done

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const nav = document.querySelector('.nav-background');
-    const navTopOffset = nav.getBoundingClientRect().top;
-  
-    window.addEventListener('scroll', function() {
-      if (window.scrollY >= navTopOffset) {
-        nav.classList.add('scrolled');
-      } else {
-        nav.classList.remove('scrolled');
-      }
-    });
+  const nav = document.querySelector('.nav-background');
+
+  function getNavOffset() {
+      return nav.getBoundingClientRect().top + window.scrollY;
+  }
+
+  window.addEventListener('scroll', function() {
+    let navTopOffset = getNavOffset();
+    if (window.scrollY >= navTopOffset) {
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
   });
+});
   
+  function toggleContent(contentId) {
+    let contentElement = document.getElementById(contentId);
+    contentElement.style.display = (contentElement.style.display === 'none' ? 'block' : 'none');
+  }
